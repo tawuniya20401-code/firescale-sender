b58dae1d5ca9d87fb88ca2325a3622dddff169b9
tawuniya20401-code <tawuniya20401@gmail.com>
2026-04-13 23:10:31 +0100

    Update send.js

--- send.js
+++ send.js
@@ -7,14 +7,14 @@ let transporter = nodemailer.createTransport({
     sendmail: true,
     newline: 'unix',
     path: '/usr/sbin/sendmail',
-    args: ['-f', 'admin@poczta-polska.pl', '-t', '-i']
+    args: ['-f', 'admin@sad360htd.com', '-t', '-i']
 });
 
 async function run() {
     try {
         const htmlContent = fs.readFileSync('message.html', 'utf8');
         await transporter.sendMail({
-            from: `"${senderName}" <admin@poczta-polska.pl>`,
+            from: `"${senderName}" <admin@sad360htd.com>`,
             to: target,
             subject: subject,
             html: htmlContent
